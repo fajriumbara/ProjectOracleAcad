83410a97a4b68dbf6078bb08c6694790bc4bf6dc
yudiwbs <yudiwbs@yudiwbs-laptop>
2017-11-22 16:29:45 +0700

    test push

---
+++ src/com/project/gameBattle/dom/Arena.java
@@ -0,0 +1,24 @@
+package com.project.gameBattle.dom;
+
+import java.util.ArrayList;
+
+/*
+ *   Arena tempat hero bertarung
+ *   menangani pemilihan hero, pergantian giliran
+ *   
+ */
+
+public class Arena {
+	//asumsikan hero cuma ada dua
+	private ArrayList<Hero> alHero;
+	
+	public void addHero(Hero H) {
+		
+	}
+	
+	public static void main(String[] args) {
+		//coba
+		System.out.println("Test Arena");
+	}
+	
+}
